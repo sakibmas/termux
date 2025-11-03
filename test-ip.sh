b5abfe711398a962315fb9ea4791de1f65ec6107
Sakibur Rahman <sakibmas@gmail.com>
2025-11-03 21:53:50 +0600

    Add nmap installation and usage for IP testing

--- test-ip.sh
+++ test-ip.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 IP=$1
 if ping -q -c 1 -W 1 google.com &>/dev/null; then
-  pkg install -y curl jq traceroute
+  pkg install -y curl jq traceroute nmap
 fi
 clear
 date
 curl -s http://ifconfig.me/all.json | jq .
 traceroute -n -m 12 $IP
-ping -c 10 $IP
+if ping -q -c 1 -W 1 $IP &>/dev/null; then
+  nmap -F --open $IP
+else
+ nmap -Pn -F --open $IP
+fi
